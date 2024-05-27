3eceb84b73f1d8c295ec89563b96c1d6fa53664f
Ikram Haddouch <haddouchikram123@gmail.com>
2024-05-27 10:31:45 +0200

    nouveau commit

--- app/src/main/java/com/example/monapplicationrepas/HomeActivity.kt
+++ app/src/main/java/com/example/monapplicationrepas/HomeActivity.kt
@@ -1,20 +1,79 @@
 package com.example.monapplicationrepas
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+
+import com.example.monapplicationrepas.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var rvAdapter: PopularAdapter
+    private lateinit var dataList:ArrayList<Recipe>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpRecyclerView()
+        binding.search.setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+        binding.salad.setOnClickListener {
+            var myIntent=Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Salad")
+            myIntent.putExtra("CATEGORY","Salad")
+            startActivity(myIntent)
+        }
+        binding.mainDish.setOnClickListener {
+            var myIntent=Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Main Dish")
+            myIntent.putExtra("CATEGORY","Dish")
+            startActivity(myIntent)
+        }
+        binding.drinks.setOnClickListener {
+            var myIntent=Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Drinks")
+            myIntent.putExtra("CATEGORY","Drinks")
+            startActivity(myIntent)
+        }
+
+        binding.desserts.setOnClickListener {
+            var myIntent=Intent(this@HomeActivity, CategoryActivity::class.java)
+            myIntent.putExtra("TITTLE","Desserts")
+            myIntent.putExtra("CATEGORY","Desserts")
+            startActivity(myIntent)
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        dataList= ArrayList()
+
+        binding.rvPolular.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        var db=Room.databaseBuilder(this@HomeActivity, AppDatabase::class.java,"db_name")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .createFromAsset("recipe.db")
+            .build()
+
+        var daoObject=db.getDao()
+        var recipes=daoObject.getAll()
+        for (i in recipes!!.indices){
+            if (recipes[i]!!.category.contains("Popular")){
+                dataList.add(recipes[i]!!)
+            }
+            rvAdapter= PopularAdapter(dataList,this)
+            binding.rvPolular.adapter=rvAdapter
         }
     }
 }
--- app/src/main/java/com/example/monapplicationrepas/PopularAdapter.kt
+++ app/src/main/java/com/example/monapplicationrepas/PopularAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.monapplicationrepas
 
-
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
@@ -29,6 +28,19 @@ class PopularAdapter(var dataList: ArrayList<Recipe>, var context: Context) :
 
         Glide.with(context).load(dataList.get(position).img).into(holder.binding.popularImg)
         holder.binding.popularTxt.text = dataList.get(position).tittle
+        var time = dataList.get(position).ing.split("\n".toRegex()).dropLastWhile { it.isEmpty() }
+            .toTypedArray()
+
+        holder.binding.popularTime.text = time.get(0)
+        holder.itemView.setOnClickListener {
+            var intent=Intent(context, RecipeActivity::class.java)
+            intent.putExtra("img",dataList.get(position).img)
+            intent.putExtra("tittle",dataList.get(position).tittle)
+            intent.putExtra("des",dataList.get(position).des)
+            intent.putExtra("ing",dataList.get(position).ing)
+            intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
+        }
 
     }
 }
