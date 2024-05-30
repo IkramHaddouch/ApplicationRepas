94a93b9829e471eed13349a65cd10f9e42cf3c25
Ikram Haddouch <haddouchikram123@gmail.com>
2024-05-30 11:58:01 +0200

    nv commit5

--- app/src/main/java/com/example/monapplicationrepas/RecipeActivity.kt
+++ app/src/main/java/com/example/monapplicationrepas/RecipeActivity.kt
@@ -1,9 +1,79 @@
 package com.example.monapplicationrepas
 
-
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
+import com.bumptech.glide.Glide
+
+import com.example.monapplicationrepas.databinding.ActivityRecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecipeBinding
+    var imgCrop=true
+    val emojiBleu = "\uD83D\uDD35" // Emoji bleu
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        Glide.with(this).load(intent.getStringExtra("img")).into(binding.itemImage)
+        binding.tittle.text=intent.getStringExtra("tittle")
+
+        binding.stepData.text=intent.getStringExtra("des")
+        var ing = intent.getStringExtra("ing")?.split("\n".toRegex())?.dropLastWhile { it.isEmpty() }
+            ?.toTypedArray()
+        binding.time.text=ing?.get(0)
+        for (i in 1 until ing!!.size){
+            binding.ingData.text=
+                """${binding.ingData.text} $emojiBleu ${ing[i]}
+                        
+                    """.trimIndent()
+
+        }
+        binding.step.background=null
+        binding.step.setTextColor(getColor(R.color.black))
+        binding.step.setOnClickListener {
+            binding.step.setBackgroundResource(R.drawable.btn_ing)
+            binding.step.setTextColor(getColor(R.color.white))
+            binding.ing.setTextColor(getColor(R.color.black))
+            binding.ing.background=null
+            binding.stepScroll.visibility=View.VISIBLE
+            binding.ingScroll.visibility=View.GONE
+
+        }
+
+        binding.ing.setOnClickListener {
+            binding.ing.setBackgroundResource(R.drawable.btn_ing)
+            binding.ing.setTextColor(getColor(R.color.white))
+            binding.step.setTextColor(getColor(R.color.black))
+            binding.step.background=null
+            binding.ingScroll.visibility=View.VISIBLE
+            binding.stepScroll.visibility=View.GONE
+        }
+        binding.fullScreen.setOnClickListener {
+
+            if (imgCrop){
+                binding.itemImage.scaleType=ImageView.ScaleType.FIT_CENTER
+                Glide.with(this).load(intent.getStringExtra("img")).into(binding.itemImage)
+                binding.fullScreen.setColorFilter(Color.BLACK,PorterDuff.Mode.SRC_ATOP)
+                binding.shade.visibility=View.GONE
+                imgCrop=!imgCrop
+            }else{
+                binding.itemImage.scaleType=ImageView.ScaleType.CENTER_CROP
+                Glide.with(this).load(intent.getStringExtra("img")).into(binding.itemImage)
+                binding.fullScreen.setColorFilter(null)
+                binding.shade.visibility=View.GONE
+                imgCrop=!imgCrop
+            }
+        }
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+
+    }
 }
